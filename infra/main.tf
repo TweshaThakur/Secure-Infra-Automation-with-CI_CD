a36956c8aaf3db623bccd097a52658a81c845ee9
TweshaThakur <tweshathakur@gmail.com>
2025-04-21 20:51:59 +0000

    Initial sync push of Terraform + To-do CI/CD setup

---
+++ infra/main.tf
@@ -0,0 +1,47 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = file("/home/rose/.ssh/id_rsa.pub")
+}
+
+resource "aws_security_group" "todo_sg" {
+  name        = "todo-sg"
+  description = "Allow SSH and app access"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "todo_instance" {
+  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 AMI in us-east-1
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.deployer.key_name
+  security_groups = [aws_security_group.todo_sg.name]
+
+  user_data = file("${path.module}/user_data.sh")
+
+  tags = {
+    Name = "ToDo-App-EC2"
+  }
+}
